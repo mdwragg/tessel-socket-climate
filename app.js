6aa724d8a7d5f3aaaf84c6da3d465edd40cacd4d
Martin <martin.wragg@ge.com>
2015-01-31 11:08:26 -0800

    more fixes...

--- app.js
+++ app.js
@@ -3,33 +3,37 @@ var router = require('tiny-router'),
     fs = require('fs'),
     tessel = require('tessel'),
     climatelib = require('climate-si7005'),
-    climate = climatelib.use(tessel.port['A']);
+    climate = climatelib.use(tessel.port['A']),
+    wifi = require('wifi-cc3000');
 
-climate.on('ready', function () {
-  router
-      .use('static', {path: './static'})
-      .use('fs', fs)
-      .listen(8080);
+wifi.on('connect', function(){
 
-  // Create a websocket server on port 8001
-  ws.createServer(function (conn) {
-    console.log("New connection")
+  climate.on('ready', function () {
+    router
+        .use('static', {path: './static'})
+        .use('fs', fs)
+        .listen(8080);
 
-    setInterval(function() {
-      climate.readTemperature('c', function (err, temp) {
-        climate.readHumidity(function (err, humid) {
-          var returnObj = {'temp': temp, 'humid': humid};
-          conn.sendText(JSON.stringify(returnObj));
+    // Create a websocket server on port 8001
+    ws.createServer(function (conn) {
+      console.log("New connection")
+
+      setInterval(function() {
+        climate.readTemperature('c', function (err, temp) {
+          climate.readHumidity(function (err, humid) {
+            var returnObj = {'temp': temp, 'humid': humid};
+            conn.sendText(JSON.stringify(returnObj));
+          });
         });
-      });
 
-    }, 2000);
+      }, 2000);
 
-    // Notify the console when the connection closes
-    conn.on("close", function (code, reason) {
-        console.log("Connection closed")
-    })
-  }).listen(8081)
+      // Notify the console when the connection closes
+      conn.on("close", function (code, reason) {
+          console.log("Connection closed")
+      })
+    }).listen(8081)
 
-  console.log('Running Server');
+    console.log('Running Server');
+  });
 });
